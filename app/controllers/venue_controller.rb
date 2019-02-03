2347a67fc77f942e3bf141b8a527cf6613763393
Khalid Shaikh <khalidx.shaikh@intel.com>
2019-02-03 13:25:02 -0800

    Display a list of seats available in react based on the JSON input

--- app/controllers/venue_controller.rb
+++ app/controllers/venue_controller.rb
@@ -1,3 +1,9 @@
 class VenueController < ApplicationController
   protect_from_forgery except: :index
+
+  def index
+    @rows = Ordway::H[:venue][:layout][:rows]
+    @columns = Ordway::H[:venue][:layout][:columns]
+  end
+
 end
---
+++ app/models/ordway.rb
@@ -0,0 +1,30 @@
+module Ordway
+  H = {
+    "venue": {
+        "layout": {
+        "rows": 10,
+        "columns": 10
+        }
+    },
+    "seats": {
+        "a1": {
+        "id": "a1",
+        "row": "a",
+        "column": 1,
+        "status": "AVAILABLE"
+        },
+        "b5": {
+        "id": "b5",
+        "row": "b",
+        "column": 5,
+        "status": "AVAILABLE"
+        },
+        "h7": {
+        "id": "h7",
+        "row": "h",
+        "column": 7,
+        "status": "AVAILABLE"
+        }
+    }
+  }
+end
